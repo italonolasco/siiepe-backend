ff1419ee90f5213ca4329a3f33ac7bbca62cfd5c
italonolasco <italonolascoramos@gmail.com>
2019-10-08 23:39:21 -0300

    Teste sem redundância de shift

--- src/controllers/ReadController.js
+++ src/controllers/ReadController.js
@@ -5,16 +5,9 @@ module.exports = {
     async store(req, res) {      
         const { reglogged } = req.headers
         const { regread } = req.params
-        const { name, userfunction, readAt } = req.body
+        const { name, userfunction, readAt, shift } = req.body
 
         let flag = false
-
-        const shiftM1 = '08:30'
-        const changeM = '10:30'
-        const shiftM2 = '12:30'
-        const shiftA1 = '13:30'
-        const changeA = '15:30'
-        const shiftA2 = '18:00'
     
         const loggedUser = await User.findOne({registration: reglogged})
         const readUser = await User.findOne({registration: regread})
@@ -23,26 +16,8 @@ module.exports = {
         const permissionRead = readUser.operator
 
         const date = new Date(readAt).toUTCString().substring(5, 16)
-        const hour = new Date(readAt).toUTCString().substring(17, 22)
-        
-        let shift = ''
-        let dateRead = ''
 
-        if(hour >= shiftM1 && hour < changeM) {
-            shift = 'm1'
-        }
-        
-        else if(hour >= changeM && hour <= shiftM2) {
-            shift = 'm2'
-        }
-        
-        else if(hour >= shiftA1 && hour < changeA) {
-            shift = 'a1'
-        }
-        
-        else if(hour >= changeA && hour <= shiftA2) {
-            shift = 'a2'
-        }
+        let dateRead = ''
 
         if((permissionLogged == "1" && permissionRead == "0") || permissionLogged == "2") {
             if(readUser.events.length != 0) {
@@ -55,7 +30,7 @@ module.exports = {
                     }
                 }, [flag])
                                
-                if(!flag && shift != '') {
+                if(!flag) {
                     readUser.events.push({
                         userfunction: userfunction,
                         name: name,
@@ -72,7 +47,7 @@ module.exports = {
                 
             }
 
-            else if (shift != '') {
+            else {
                 readUser.events.push({
                     userfunction: userfunction,
                     name: name,
